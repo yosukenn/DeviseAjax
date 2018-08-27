71281880acb48e2b2c878f67a651c72c10b38552
田中陽祐 <lo.sa.wo.10969.already@gmail.com>
2018-08-28 00:47:33 +0900

    test form_with

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 
 ActiveRecord::Schema.define(version: 20180827143835) do
 
-  create_table "hellos", force: :cascade do |t|
+  create_table "hellos", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "greet", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
